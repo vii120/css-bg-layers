603f09c3419e607e4fb2e9093d20a94385af77b1
Vivi Tseng <vii0726@gmail.com>
2026-03-22 20:12:47 -0700

    feat: remove sessionStorage

--- lib/store.ts
+++ lib/store.ts
@@ -1,20 +1,11 @@
 import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface CssStore {
   css: string
   setCss: (css: string) => void
 }
 
-export const useCssStore = create<CssStore>()(
-  persist(
-    (set) => ({
-      css: '',
-      setCss: (css) => set({ css }),
-    }),
-    {
-      name: 'layer-css',
-      storage: createJSONStorage(() => sessionStorage),
-    },
-  ),
-)
+export const useCssStore = create<CssStore>()((set) => ({
+  css: '',
+  setCss: (css) => set({ css }),
+}))
